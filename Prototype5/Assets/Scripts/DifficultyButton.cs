3a85846fb649bf88c5a847a8ff886497fc7789ea
J-bit812 <142627732+J-bit812@users.noreply.github.com>
2024-04-10 13:25:49 -0700

    Removed Unused Libraries, Serialized Public Fields.

--- Prototype5/Assets/Scripts/DifficultyButton.cs
+++ Prototype5/Assets/Scripts/DifficultyButton.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,7 +5,7 @@ public class DifficultyButton : MonoBehaviour
 {
     private Button button;
     private GameManager gameManager;
-    public int difficulty;
+    [SerializeField] private int difficulty;
     // Start is called before the first frame update
     void Start()
     {

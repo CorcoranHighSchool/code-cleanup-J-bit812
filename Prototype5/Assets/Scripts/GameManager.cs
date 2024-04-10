babda13df8c79773dad6ba96dc9cbbd408774c73
J-bit812 <142627732+J-bit812@users.noreply.github.com>
2024-04-10 13:30:30 -0700

    Removed Unused LIbraries, Removed Empty Methods, Serialized Public Fields, Converted Strings to Constants.

--- Prototype5/Assets/Scripts/GameManager.cs
+++ Prototype5/Assets/Scripts/GameManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -7,13 +6,14 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private int score = 0;
-    public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI gameOverText;
-    public GameObject titleScreen;
-    public Button restartButton;
-    public List<GameObject> targets;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI gameOverText;
+    [SerializeField] private GameObject titleScreen;
+    [SerializeField] private Button restartButton;
+    [SerializeField] private List<GameObject> targets;
     private float spawnRate = 1.0f;
-    public bool isGameActive = false;
+    [SerializeField] private bool isGameActive = false;
+    private const string score = "Score: ";
 
     public void StartGame(int difficulty)
     {
@@ -25,11 +25,7 @@ public class GameManager : MonoBehaviour
         UpdateScore(0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
     private IEnumerator SpawnTarget()
     {
         while (isGameActive)
@@ -42,7 +38,7 @@ public class GameManager : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "Score: " + score;
+        scoreText.text = score + score;
     }
     public void GameOver()
     {
